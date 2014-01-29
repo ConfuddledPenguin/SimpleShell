bad4deb7b3da0ed04ec610f1fb7782689894676a
Tom Maxwell <2tommax@gmail.com>
2014-01-29 15:50:03 +0000

    File birth and README.md filled

---
+++ README.md
@@ -0,0 +1,21 @@
+#Simple Shell v0.0 28/1/2014#
+
+###Authors:##
+
+Thomas Maxwell		- gvb12182
+
+Thomas Sinclair    	- mlb12174
+
+Aidan O'Grady       - wlb12153
+
+Grant Toghill       - wsb12157
+
+###Description:###
+
+
+This is a simple unix like shell written in C, for the class CS210 at the University of StrathCylde. This was a group project to be undertaken by 4 students.
+
+###LICENSE:###
+
+All of the content of this repository belongs the person who wrote it. Only they have permission to copy and distribute the content.
+
---
+++ SimpleShell.c
@@ -0,0 +1,39 @@
+/******************************************************************************
+ * Synopsis: This is a simple unix like shell written in C, for the class CS210 
+ *      at the University of Strathcylde. This was a group project to be 
+ *      undertaken by four students.
+ *
+ * Compiling: This program should be compiled using the C99 standard. This is
+ *      due to the inclusion of c++ style comments i.e: //comment
+ *      The flag for this is: -std=c99
+ *
+ * Bugs:
+ *      For past bugs please see Revision History
+ *
+ * Authors:
+ *      Thomas Maxwell     - gvb12182
+ *      Thomas Sinclair    - mlb12174
+ *      Aidan O'Grady      - wlb12153
+ *      Grant Toghill      - wsb12157
+ *
+ * Version:
+ *      See VERSION below
+ *
+ *****************************************************************************/
+
+/********************* R E V I S I O N   H I S T O R Y *************************
+ * 
+ * v0.0 - 28/01/2014 - File birth
+ *
+ *      Created initial file (SimpleShell.c) as well as read me files and a
+ *      github repo.
+ *
+ ******************************************************************************/
+ #define VERSION "v0.0. Last Update 28/01/2014\n"
+
+ #include <stdio.h>
+
+ int main(){
+
+  return(0);
+ }
