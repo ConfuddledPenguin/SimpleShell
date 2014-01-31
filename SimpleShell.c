e0b0d0ed6e959897ee587228f09ee2fe611a6e6a
Tom Maxwell <2tommax@gmail.com>
2014-01-31 15:26:48 +0000

    Continuous loop, user input, and tokenising

--- SimpleShell.c
+++ SimpleShell.c
@@ -33,22 +33,53 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <windows.h>
+
+typedef struct
+{
+	char *command;
+} user_command;
 
 int main(){
 
+	//variable
 	int run = 1;
-	char command[512];
+	char *prompt = "> ";
+	char input[512];
 	char *p;
 
-	while (run){
+	//user loop
+	while (run || (getchar() != EOF) ){
 
- 		printf("> ");
- 		fgets(command, 512, stdin);
- 		printf("%s", command);
+		user_command command = {
+			NULL
+		};
 
- 		if ((p = strchr(command, '\n')) != NULL)
-    		p = '\0';
- 		run = 0;
+		//prompt user
+ 		printf("%s", prompt);
+ 		fgets(input, 512, stdin);
+
+ 		//Simple check
+ 		printf("The command: %s", input);
+
+ 		//Getting rid of the new line char, replacing with a terminating char
+ 		if ((p = strchr(input, '\n')) != NULL)
+ 			*p = '\0';
+
+ 		//TOKENISE HERE
+ 		char *tokenizer = " ";
+ 		char *token;
+ 		command.command = strtok(input, tokenizer);
+ 		printf("%s\n", command);
+ 		while ( (token = strtok(NULL, tokenizer) ) != NULL){
+
+ 			printf("%s\n", token);
+ 		}
+
+ 		//exit check
+    	if(strcmp(command.command, "exit") == 0){ 
+ 			run = 0;
+ 		}
  	}
 
  return(0);
